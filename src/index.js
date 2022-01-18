b7b7e7b0a14bf3f98ddc71798af5658255e149aa
leidy tapias <leidytapiasgiraldo@gmail.com>
2022-01-18 18:39:10 -0500

    se realiza el validador de js y el index js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-import validator from './validator.js';
+import validator from "./validator.js";
+const mostrarMensaje = (e) => {
+  e.preventDefault();
+  const numeroDetarjeta = document.getElementById("grupo__password").value;
 
-console.log(validator());
+  if (numeroDetarjeta == "" || numeroDetarjeta == null) {
+    console.log("por favor diligencia todos los campos");
+    error.style.display = "block";
+    error.innerHTML += "por favor diligencia todos los campos";
+  }
+  if (numeroDetarjeta.length == 16) {
+    todo_correcto = false;
+  }
+  if (isNaN(numeroDetarjeta)) {
+    
+    todo_correcto = false;
+  }
+
+  console.log(validator.isValid(numeroDetarjeta));
+};
+formulario.addEventListener("submit", mostrarMensaje);
--- src/validator.js
+++ src/validator.js
@@ -1,6 +1,34 @@
 const validator = {
-//   ...
-};
-const numero = '4137894711755904' 
+isValid: (numeroDetarjetaEnletras) => {
+
+   if(isNaN (numeroDetarjetaEnletras)){
+       return false;
+   }
+   const arregloDenumeros = numeroDetarjetaEnletras.split("");
+   let convertirAnumero = numeroDetarjetaEnletras.map(numero => parseInt(numero)
+   );     
+   convertirAnumero.map((numero, indice) => {
+       if(indice % 2 == 0) {
+           numero = numero*2;
+       } 
+       return numero;
+   });
+
+    let sumatotal = numero.reduce((a,b) => a+b, 0);{
+      
+        return sumatotal % 10 == 0;
+
+
+
+    }
+
+ 
+}   
+   
+
+    }
+
+  
+ console.log(total);
 
 export default validator;
