7d8587a6258e259c78801ced44738e155a6915d2
leidy tapias <leidytapiasgiraldo@gmail.com>
2022-01-25 12:28:25 -0500

    se agrega comentarios al codigo

--- src/validator.js
+++ src/validator.js
@@ -4,12 +4,13 @@ const validator = {
     if (isNaN(numeroDetarjetaEnletras)) {
       return false;
     }
-
     // una ves se recibe el numero y se convierte en un arregle de string
     const arregloDenumeros = numeroDetarjetaEnletras.split("");
     let convertirAnumero = arregloDenumeros.map((numero) => parseInt(numero));
     console.log(convertirAnumero);
+    // Crea un arreglo con números en posiciones pares multiplicados por dos
     const arregloMultiplicadoPorDos = convertirAnumero.map((numero, indice) => {
+      // Verifica que la posición sea par
       if (indice % 2 == 0) {
         numero = numero * 2;
         return numero;
@@ -18,11 +19,15 @@ const validator = {
       }
     });
     console.log(arregloMultiplicadoPorDos);
+    // Crea un arreglo con números en posiciones pares mayores a 10 sumando sus digitos
     const arregloMayorADiez = arregloMultiplicadoPorDos.map((numero, indice) => {
+      // Verifica que la posición sea par
       if (indice % 2 == 0) {
+        // Verifica que el número en la posición par sea mayor o igual a 10
         if (numero >= 10) {
           const digitos = numero.toString().split(""); // 14 => ["1", "4"];
-          const digitosEnNumeros = digitos.map((numero) => parseInt(numero)); // [1, 4];
+          const digitosEnNumeros = digitos.map((numero) => parseInt(numero)); // ["1", "4"] => [1, 4];
+          // Suma los digitos del número: [1, 4] => 5
           let sumadigitos = digitosEnNumeros.reduce((a, b) => a + b);
           numero = sumadigitos;
         }
@@ -32,8 +37,10 @@ const validator = {
       }
     });
     console.log(arregloMayorADiez);
+    // Suma el total de los números del arreglo anterior
     let sumatotal = arregloMayorADiez.reduce((a, b) => a + b);
     console.log(sumatotal);
+    // Verifica si la suma total es multiplo de 10
     return sumatotal % 10 == 0;
   },
 };
