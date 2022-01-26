e1df42bffe2eb1e875ff86ed89acffdac3d299a7
leidy tapias <leidytapiasgiraldo@gmail.com>
2022-01-25 20:48:09 -0500

    se realiza innert text para mensaje al usuario

--- src/index.js
+++ src/index.js
@@ -3,21 +3,27 @@ const mostrarMensaje = (e) => {
   e.preventDefault();
   const numeroDetarjeta = document.getElementById("numeroTarjeta").value;
   const error = document.getElementById("error");
+  const respuesta = document.getElementById("respuesta");
+  respuesta.innerText = "";
   if (numeroDetarjeta == "" || numeroDetarjeta == null) {
     console.log("<li>por favor diligencia todos los campos;</li>");
     error.style.display = "block";
     error.innerHTML += "<li>por favor diligencia todos los campos;</li>";
-  }
-  if (numeroDetarjeta.length < 16) {
+  } else if (numeroDetarjeta.length < 16) {
     error.style.display = "block";
     error.innerHTML += "<li>Número tarjeta pequeño;</li>";
-  }
-  if (isNaN(numeroDetarjeta)) {
+  } else if (isNaN(numeroDetarjeta)) {
     error.style.display = "block";
     error.innerHTML += "<li>El número de tarjeta no es un número;</li>";
+  } else {
+    const esValido = validator.isValid(numeroDetarjeta);
+
+    if (esValido) {
+      respuesta.innerText = "!El número de tarjeta es válido!";
+    } else {
+      respuesta.innerText = "!El número de tarjeta no es válido!";
+    }
   }
-  const esValido = validator.isValid(numeroDetarjeta);
-  console.log(esValido);
 };
 const formulario = document.getElementById("formulario");
 formulario.addEventListener("submit", mostrarMensaje);
