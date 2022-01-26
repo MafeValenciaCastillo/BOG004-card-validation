d269fad71647655cde1248c1873a7340cee45221
Mafe valencia <mariafvc22@gmail.com>
2022-01-26 09:47:44 -0500

    cambios pequeños en el index.js

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ const mostrarMensaje = (e) => {
   respuesta.innerText = "";
   if (numeroDetarjeta == "" || numeroDetarjeta == null) {
     console.log("<li>por favor diligencia todos los campos</li>");
-    error.style.display = "block";
+    error.style.display = "none";
     error.innerHTML += "<li>por favor diligencia todos los campos</li>";
   } else if (numeroDetarjeta.length < 16) {
-    error.style.display = "block";
-    error.innerHTML += "<li>Número tarjeta pequeño;</li>";
+    error.style.display = "none";
+    error.innerHTML += "<li>Número tarjeta demasiado corto</li>";
   } else if (isNaN(numeroDetarjeta)) {
-    error.style.display = "block";
+    error.style.display = "none";
     error.innerHTML += "<li>El número de tarjeta no es un número</li>";
   } else {
     const esValido = validator.isValid(numeroDetarjeta);
