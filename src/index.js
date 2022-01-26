e4f949fc01c6928f30e6a1ac5a308d75da18207e
Mafe valencia <mariafvc22@gmail.com>
2022-01-26 09:28:09 -0500

    ajustes index.js

--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@ const mostrarMensaje = (e) => {
   const respuesta = document.getElementById("respuesta");
   respuesta.innerText = "";
   if (numeroDetarjeta == "" || numeroDetarjeta == null) {
-    console.log("<li>por favor diligencia todos los campos;</li>");
+    console.log("<li>por favor diligencia todos los campos</li>");
     error.style.display = "block";
-    error.innerHTML += "<li>por favor diligencia todos los campos;</li>";
+    error.innerHTML += "<li>por favor diligencia todos los campos</li>";
   } else if (numeroDetarjeta.length < 16) {
     error.style.display = "block";
     error.innerHTML += "<li>Número tarjeta pequeño;</li>";
   } else if (isNaN(numeroDetarjeta)) {
     error.style.display = "block";
-    error.innerHTML += "<li>El número de tarjeta no es un número;</li>";
+    error.innerHTML += "<li>El número de tarjeta no es un número</li>";
   } else {
     const esValido = validator.isValid(numeroDetarjeta);
 
